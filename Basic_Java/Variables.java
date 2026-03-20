fd1a1fea1dd8ff486662ea3c62ea1705cf09ec5a
devps814 <dev.singh_cs22@gla.ac.in>
2026-03-20 11:40:52 +0530

    defining and some sample code of Variables for practice

---
+++ Basic_Java/Variables.java
@@ -0,0 +1,21 @@
+class variable {
+    public static void main(String[] args) {
+        // A variable is a named location in memory used to store data. You can think of
+        // it as a pointer or a label for a value that might change during the execution
+        // of a program.
+
+        // Declaration
+        // Telling the computer to set aside space.
+        int age;
+
+        // Initialization
+        // Giving that space its first value.
+        age = 22;
+
+        // Naming Conventions
+        // Usually, variables follow "camelCase" and cannot start with numbers.
+        String fullName;
+        fullName = "Dev Pratap Singh";
+
+    }
+}
