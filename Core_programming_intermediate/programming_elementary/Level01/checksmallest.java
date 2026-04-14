98b29bcb9c503fb43794ab99223b77382d12167c
devps814 <dev.singh_cs22@gla.ac.in>
2026-04-14 11:57:26 +0530

    check the samllest sumber

---
+++ Core_programming_intermediate/programming_elementary/Level01/checksmallest.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class checksmallest {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		int num3 = sc.nextInt();
+		
+		if (num1 < num2 ) {
+			if (num1 < num3 ) {
+				System.out.println(" Is the first number the smallest? Yes ");
+			}
+			else {
+				System.out.println(" Is the first number the smallest? No ");
+			}
+		}
+	}
+
+}
