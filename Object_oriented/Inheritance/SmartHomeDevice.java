726495141f3ac03234a7ec579700536db2678192
devps814 <dev.singh_cs22@gla.ac.in>
2026-05-04 12:37:18 +0530

    create class Device and its subclass Thermostat

---
+++ Object_oriented/Inheritance/SmartHomeDevice.java
@@ -0,0 +1,36 @@
+
+class Device {
+    private String deviceId;
+    private String status;
+
+    public Device(String deviceId, String status) {
+        this.deviceId = deviceId;
+        this.status = status;
+    }
+
+    public void displayStatus() {
+        System.out.println("Device ID: " + deviceId + ", Status: " + status);
+    }
+}
+
+class Thermostat extends Device {
+    private double temperatureSetting;
+
+    public Thermostat(String deviceId, String status, double temperatureSetting) {
+        super(deviceId, status);
+        this.temperatureSetting = temperatureSetting;
+    }
+
+    @Override
+    public void displayStatus() {
+        super.displayStatus();
+        System.out.println("Temperature Setting: " + temperatureSetting + "°C");
+    }
+}
+
+public class SmartHomeDevice {
+    public static void main(String[] args) {
+        Thermostat t = new Thermostat("T1001", "ON", 22.5);
+        t.displayStatus();
+    }
+}
