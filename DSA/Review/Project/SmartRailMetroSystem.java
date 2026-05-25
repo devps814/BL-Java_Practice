e14d76d959b9512ee1b104bdbfd6ab2d2fec3cd6
Dev Pratap Singh <122431646+devps814@users.noreply.github.com>
2026-05-25 16:45:44 +0530

    Delete DSA directory
    It already has separate branch and their specific content

--- DSA/Review/Project/SmartRailMetroSystem.java
+++
@@ -1,7 +0,0 @@
-
-public class SmartRailMetroSystem {
-    public static void main(String[] args) {
-        PassengerRegistration pg = new PassengerRegistration(236, "Abhay", 24, "Rajiv_Chawk", "Kashmiri_gate", 50);
-        pg.addPassenger(pg);
-    }
-}
